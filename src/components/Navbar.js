2040b0c39f3f662f427edcf8f774df70d0567f3b
NotHamada <matheushamada@live.com>
2023-06-10 16:40:09 -0300

    Adding some files, created some css for the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,44 @@
 import "bootstrap";
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import "./Navbar.css";
+import ReactDOM from "react-dom";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-purple bg-soft-lilac">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          <h2 class="fw-bold">Hello, I'm Matheus Hamada</h2>
         </a>
         <button
           class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-bs-target="#navbar"
+          aria-controls="navbar"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbar">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
+              <a class="nav-link" aria-current="page" href="#">
+                About me
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
-                Link
+                Experiences
               </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a class="nav-link" href="#">
+                Contacts
+              </a>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
